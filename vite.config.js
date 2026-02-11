f1b6d018961f9163be5c2dc8a58d54926a4d12de
brwndo <hi@brandonarthurroth.com>
2026-02-11 13:29:32 -0700

    Updated server configuration for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/crunch-navigation/', // must match repo name for GitHub Pages
   plugins: [react()],
 });
