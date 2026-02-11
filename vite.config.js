e6989b536b8b83fc41957944ddce2a2e8e2f8991
brwndo <hi@brandonarthurroth.com>
2026-02-11 13:35:04 -0700

    Fix GitHub Pages base path for nav-wires repo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/crunch-navigation/', // must match repo name for GitHub Pages
+  base: '/nav-wires/', // must match repo name for GitHub Pages
   plugins: [react()],
 });
